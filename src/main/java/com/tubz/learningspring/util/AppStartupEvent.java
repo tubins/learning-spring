327ab3919c831fb949dc766e7013ec2c746b9d06
tubins <tubin.shaji@gmail.com>
2022-04-26 21:24:43 +0100

    Verify the data is loaded using app start event listener

--- src/main/java/com/tubz/learningspring/util/AppStartupEvent.java
+++ src/main/java/com/tubz/learningspring/util/AppStartupEvent.java
@@ -1,7 +1,6 @@
 package com.tubz.learningspring.util;
 
-import com.tubz.learningspring.data.Room;
-import com.tubz.learningspring.data.RoomRepository;
+import com.tubz.learningspring.data.*;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Component;
@@ -9,14 +8,22 @@ import org.springframework.stereotype.Component;
 @Component
 public class AppStartupEvent implements ApplicationListener<ApplicationReadyEvent> {
     final private RoomRepository roomRepository;
+    final private GuestRepository guestRepository;
+    final private ReservationRepository reservationRepository;
 
-    public AppStartupEvent(RoomRepository roomRepository) {
+    public AppStartupEvent(RoomRepository roomRepository, GuestRepository guestRepository, ReservationRepository reservationRepository) {
         this.roomRepository = roomRepository;
+        this.guestRepository = guestRepository;
+        this.reservationRepository = reservationRepository;
     }
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
         Iterable<Room> rooms = this.roomRepository.findAll();
         rooms.forEach(System.out::println);
+        Iterable<Guest> guests = this.guestRepository.findAll();
+        guests.forEach(System.out::println);
+        Iterable<Reservation> reservations = reservationRepository.findAll();
+        reservations.forEach(System.out::println);
     }
 }
