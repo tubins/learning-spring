b536a5f79475c713e5edaaa4085f4368c5dc5d65
tubins <tubin.shaji@gmail.com>
2022-04-26 21:20:03 +0100

    Add Guest and Reservation classes and it repositories

---
+++ src/main/java/com/tubz/learningspring/data/Guest.java
@@ -0,0 +1,36 @@
+package com.tubz.learningspring.data;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+/**
+ * Guest class.
+ */
+@Entity
+@Table(name = "GUEST")
+@Getter
+@Setter
+@ToString
+public class Guest {
+    @Id
+    @Column(name = "GUEST_ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long guestId;
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "LAST_NAME")
+    private String lastName;
+    @Column(name = "EMAIL_ADDRESS")
+    private String emailAddress;
+    @Column(name = "ADDRESS")
+    private String address;
+    @Column(name = "COUNTRY")
+    private String country;
+    @Column(name = "STATE")
+    private String state;
+    @Column(name = "PHONE_NUMBER")
+    private String phoneNumber;
+}
