38531373ec5336a2fedd7fa8a133d6d8953e8831
tubins <tubin.shaji@gmail.com>
2022-04-26 23:07:00 +0100

    Add Reservation Service layer

--- src/main/java/com/tubz/learningspring/data/Room.java
+++ src/main/java/com/tubz/learningspring/data/Room.java
@@ -21,6 +21,8 @@ public class Room {
     private Long id;
     @Column(name = "NAME")
     private String name;
+    @Column(name = "ROOM_NUMBER")
+    private String roomNumber;
     @Column(name = "BED_INFO")
     private String bedInfo;
 }
