2da3d67a433bc9a2c37ec7430ee012372dcfe072
tubins <tubin.shaji@gmail.com>
2022-04-27 15:57:22 +0100

    Add Reservation MVC

---
+++ src/main/java/com/tubz/learningspring/web/RoomReservationController.java
@@ -0,0 +1,33 @@
+package com.tubz.learningspring.web;
+
+import com.tubz.learningspring.business.ReservationService;
+import com.tubz.learningspring.business.RoomReservation;
+import com.tubz.learningspring.util.DateUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Date;
+import java.util.List;
+
+@Controller
+@RequestMapping("/reservations")
+public class RoomReservationController {
+    private final DateUtils dateUtils;
+    private final ReservationService reservationService;
+
+    public RoomReservationController(DateUtils dateUtils, ReservationService reservationService) {
+        this.dateUtils = dateUtils;
+        this.reservationService = reservationService;
+    }
+
+    @GetMapping()
+    public String getReservations(@RequestParam(value = "date", required = false) String dateString, Model model) {
+        Date date = this.dateUtils.createDateFromDateString(dateString);
+        List<RoomReservation> roomReservations = this.reservationService.getRoomReservationsForDate(date);
+        model.addAttribute("roomReservations", roomReservations);
+        return "roomres";
+    }
+}
